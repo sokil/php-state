25a80e3d0aef3f9e78802b79cb9a423829e0590c
Dmytro Sokil <dmytro.sokil@gmail.com>
2015-08-06 17:55:00 +0300

    state test

---
+++ tests/StateTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Sokil\State;
+
+class StateTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetMetadata()
+    {
+        $state = new State('stateName', [
+            'param1' => 'value1',
+            'param2' => 'value2',
+        ]);
+
+        $this->assertEquals([
+            'param1' => 'value1',
+            'param2' => 'value2',
+        ], $state->getMetadata());
+
+        $this->assertEquals('value1', $state->getMetadata('param1'));
+        $this->assertEquals('value2', $state->getMetadata('param2'));
+    }
+}
